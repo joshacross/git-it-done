b184d70df1af2e2e5bfe340e1a5ce978b2bc104e
Killa J <joshuaalexandercross@gmail.com>
2021-03-18 00:07:06 -0500

    added form field to html, eventlisteners to JS, and ppassed data to getUserRepos function

--- assets/js/hompage.js
+++ assets/js/hompage.js
@@ -1,3 +1,21 @@
+var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#username");
+
+var formSubmitHandler = function(event) {
+  event.preventDefault();
+
+  var username = nameInputEl.value.trim();
+
+  if (username) {
+    getUserRepos(username);
+    nameInputEl.value = "";
+  } else {
+    alert("Please enter a GitHub username");
+  }
+};
+
+userFormEl.addEventListener("submit", formSubmitHandler);
+
 var getUserRepos = function(user) {
 
   // format the github api url
