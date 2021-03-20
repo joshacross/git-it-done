b2fa856a422d861fc58aa141feabba29ec4e935e
Killa J <joshuaalexandercross@gmail.com>
2021-03-19 22:32:01 -0500

    fetch request to GitHub repository issues

---
+++ assets/js/single.js
@@ -0,0 +1,19 @@
+var getRepoIssues = function(repo) {
+    var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
+    console.log(apiUrl);
+
+    fetch(apiUrl)
+    .then(function(response) {
+        // request was successful
+        if (response.ok) {
+            response.json()
+            .then(function(data) {
+                console.log(data);
+            });
+        } else {
+            alert("there was a problem with your request");
+        }
+    });
+};
+
+getRepoIssues("facebook/react");
