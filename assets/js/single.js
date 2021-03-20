c0dd533d952b1a09e864993f5667b4005cbc2546
Killa J <joshuaalexandercross@gmail.com>
2021-03-20 00:48:32 -0500

    connected secondary page to primary index and split strings

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,5 +1,24 @@
+var repoNameEl = document.querySelector("#repo-name");
 var issueContainerEl = document.querySelector("#issues-container");
 // var limitWarningEl = document.querySelector("#limit-warning");
+
+
+var getRepoName = function() {
+    // grab repo name from url query string
+    var queryString = document.location.search;
+    var repoName = queryString.split("=")[1];
+
+    if (repoName) {
+
+        // display repo name on the page
+        repoNameEl.textContent = repoName;
+
+        getRepoIssues(repoName);
+    } else {
+        // if no repo was given, redirect to homepage
+        document.location.replace("./index.html");
+    }
+};
 
 var getRepoIssues = function(repo) {
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
@@ -20,7 +39,8 @@ var getRepoIssues = function(repo) {
             }
             });
         } else {
-            alert("there was a problem with your request");
+            // if not successful, redirect to homepage
+            document.location.replace("./index.html");
         }
     });
 };
@@ -80,4 +100,4 @@ var displayWarning = function(repo) {
     limitWarningEl.appendChild(linkEl);
   };
 
-getRepoIssues("facebook/react");
+getRepoName();
