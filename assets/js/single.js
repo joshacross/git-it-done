82367120891f02a7285aed55374a3324b7aa53d9
Killa J <joshuaalexandercross@gmail.com>
2021-03-20 00:06:02 -0500

    added header link to issues

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,4 +1,5 @@
 var issueContainerEl = document.querySelector("#issues-container");
+// var limitWarningEl = document.querySelector("#limit-warning");
 
 var getRepoIssues = function(repo) {
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
@@ -12,6 +13,11 @@ var getRepoIssues = function(repo) {
             .then(function(data) {
             // pass response data to dom function
             displayIssues(data);
+
+            // check if api has paginated issues
+            if (response.headers.get("Link")) {
+                displayWarning(repo);
+            }
             });
         } else {
             alert("there was a problem with your request");
@@ -56,5 +62,22 @@ var displayIssues = function(issues) {
         issueContainerEl.appendChild(issueEl);
     }
 };
+
+var displayWarning = function(repo) {
+    
+    var limitWarningEl = document.querySelector("#limit-warning");
+    
+    // add text to warning container
+    limitWarningEl.textContent = "To see more than 30 issues, visit ";
+  
+    // create link element
+    var linkEl = document.createElement("a");
+    linkEl.textContent = "GitHub.com";
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+    linkEl.setAttribute("target", "_blank");
+  
+    // append to warning container
+    limitWarningEl.appendChild(linkEl);
+  };
 
 getRepoIssues("facebook/react");
